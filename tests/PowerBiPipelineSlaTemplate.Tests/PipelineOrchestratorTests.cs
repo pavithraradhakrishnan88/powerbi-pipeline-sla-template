6df071a6d436c08202b0be2e803f3cbb3be3aa74
pavithraradhakrishnan88 <pavithraradhakrishnan02@gmail.com>
2026-08-15 15:12:04 +0530

    Cover semantic-model template/output path separation

--- tests/PowerBiPipelineSlaTemplate.Tests/PipelineOrchestratorTests.cs
+++ tests/PowerBiPipelineSlaTemplate.Tests/PipelineOrchestratorTests.cs
@@ -31,6 +31,7 @@ public class PipelineOrchestratorTests
         var options = new PipelineOptions
         {
             DataDirectoryPath = Path.Combine(fixture.DataDirectoryPath, "does-not-exist"),
+            SemanticModelTemplateRootPath = fixture.Options.SemanticModelTemplateRootPath,
             SemanticModelRootPath = fixture.Options.SemanticModelRootPath,
             ReportRootPath = fixture.Options.ReportRootPath,
             ReportTemplateRootPath = fixture.Options.ReportTemplateRootPath,
@@ -53,6 +54,7 @@ public class PipelineOrchestratorTests
         var options = new PipelineOptions
         {
             DataDirectoryPath = fixture.Options.DataDirectoryPath,
+            SemanticModelTemplateRootPath = fixture.Options.SemanticModelTemplateRootPath,
             SemanticModelRootPath = fixture.Options.SemanticModelRootPath,
             ReportRootPath = fixture.Options.ReportRootPath,
             ReportTemplateRootPath = Path.Combine(fixture.ReportTemplateRootPath, "missing"),
@@ -77,5 +79,29 @@ public class PipelineOrchestratorTests
         Action act = () => orchestrator.Run(fixture.Options);
 
         act.Should().Throw<InvalidDataException>();
+    }
+
+    [Fact]
+    public void Run_ShouldThrow_WhenSemanticModelTemplateAndOutputPathsAreIdentical()
+    {
+        using var fixture = new PipelineTestFixture("Normal");
+        var options = new PipelineOptions
+        {
+            DataDirectoryPath = fixture.Options.DataDirectoryPath,
+            SemanticModelTemplateRootPath = fixture.Options.SemanticModelTemplateRootPath,
+            SemanticModelRootPath = fixture.Options.SemanticModelTemplateRootPath,
+            ReportRootPath = fixture.Options.ReportRootPath,
+            ReportTemplateRootPath = fixture.Options.ReportTemplateRootPath,
+            SemanticModelRelativePath = fixture.Options.SemanticModelRelativePath,
+            MetadataOutputPath = fixture.Options.MetadataOutputPath,
+            ThrowOnValidationError = fixture.Options.ThrowOnValidationError,
+            Logger = fixture.Options.Logger
+        };
+        var orchestrator = new PipelineOrchestrator();
+
+        Action act = () => orchestrator.Run(options);
+
+        act.Should().Throw<InvalidOperationException>()
+            .WithMessage("*SemanticModelTemplateRootPath and SemanticModelRootPath must be different*");
     }
 }
